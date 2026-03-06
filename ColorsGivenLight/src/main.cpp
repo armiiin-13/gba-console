9e8596ff7ad4235060adf0bacad58432bb899e8a
armin <armingarc@yahoo.com>
2026-03-06 10:42:38 +0100

    Added colors exercise implementation

--- ColorsGivenLight/src/main.cpp
+++ ColorsGivenLight/src/main.cpp
@@ -1,18 +1,93 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// pin declaration
+int button = 13;
+int ledRed = 6;
+int ledGreen = 5;
+int ledBlue = 3;
+int ldr = A0;
+
+int valueRed = 0;
+int valueGreen = 123;
+int valueBlue = 0;
+
+int umbral = 600;
+
+// functions declaration
+void setUpLed();
+void upToRed();
+void upToBlue();
+void printRGBValues();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledRed,OUTPUT);
+  pinMode(ledGreen,OUTPUT);
+  pinMode(ledBlue,OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(ldr,INPUT);
+
+  setUpLed();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int ldrValue = analogRead(ldr);
+  Serial.print("LDR Value = ");
+  Serial.println(ldrValue);
+  delay(500);
+  int buttonValue = digitalRead(button);
+  if (buttonValue == HIGH){ // button pressed
+    if (ldrValue < umbral){ // tendency to blue
+      for (int i = 0; i < 5; i++){
+        upToBlue();
+        printRGBValues();
+        delay(500);
+      }
+    } else { // tendency to red
+      for (int i = 0; i < 5; i++){
+        upToRed();
+        printRGBValues();
+        delay(200);
+      }
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setUpLed(){
+  analogWrite(ledRed, valueRed);
+  analogWrite(ledGreen, valueGreen);
+  analogWrite(ledBlue, valueBlue);
+}
+
+void upToRed(){
+  if (valueRed < 255){
+    valueRed += 51;
+    analogWrite(ledRed, valueRed);
+  }
+  if (valueBlue > 0){
+    valueBlue -= 51;
+    analogWrite(ledBlue, valueBlue);
+  }
+}
+
+void upToBlue(){
+  if (valueBlue < 255){
+    valueBlue += 51;
+    analogWrite(ledBlue, valueBlue);
+  }
+  if (valueRed > 0){
+    valueRed -= 51;
+    analogWrite(valueRed, valueRed);
+  }
+}
+
+void printRGBValues(){
+  Serial.print("RGB Led = (");
+  Serial.print(valueRed);
+  Serial.print(", ");
+  Serial.print(valueGreen);
+  Serial.print(", ");
+  Serial.print(valueBlue);
+  Serial.println(")");
 }

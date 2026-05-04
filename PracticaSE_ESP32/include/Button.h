80e4bfd4876d6d836a9ba7892ca2c8dca77875b1
armin <armingarc@yahoo.com>
2026-05-04 21:47:35 +0200

    Comments to english

--- PracticaSE_ESP32/include/Button.h
+++ PracticaSE_ESP32/include/Button.h
@@ -4,17 +4,17 @@
 #include <Arduino.h>
 
 class Button {
-    private:                                    // variables que solo pueden ser modificadas por la propia clase
-        uint8_t _pin;                           // pin (placa) al que esta conectado el boton
-        bool _lastState;                        // estado actual y ultimo para 
+    private:                                    
+        uint8_t _pin;                           // pin connected
+        bool _lastState;                        // last state read
         bool _stableState;
-        unsigned long _lastDebouncetime;        // guarda el tiempo de la ultima vez que el pin cambio de estado
+        unsigned long _lastDebouncetime;        // time when state last changed
         unsigned long _pressStartTime;
         unsigned long _debounceDelay = 50;
 
     public:
         Button(u_int8_t);                       // constructor
-        void begin();                           // pinMode en INPUT_PULLUP
+        void begin();                           // pinMode in INPUT_PULLUP
         bool isPressed();                       
         bool isLongPressed();
 };
